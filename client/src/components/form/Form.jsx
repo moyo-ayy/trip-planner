fd0f3f73b44f3777d72fe0c1d15099cf2bfc476f
lindaislinda <thaonguyenntp2003@gmail.com>
2023-09-16 21:13:00 -0400

    Preferences page completed

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { TextField, Button, Box } from "@mui/material";
+import { TextField, Button, Box, Typography } from "@mui/material";
+import "./form.css";
 
 function Form() {
   const [formData, setFormData] = useState({
@@ -7,14 +8,17 @@ function Form() {
     to: "",
     days: "",
     food: [],
+    hobbies: [],
+    places: [],
+    other: "",
   });
 
-  const toggleFoodSelection = (food) => {
+  const toggleSelection = (field, value) => {
     setFormData((prevState) => ({
       ...prevState,
-      food: prevState.food.includes(food)
-        ? prevState.food.filter((f) => f !== food)
-        : [...prevState.food, food],
+      [field]: prevState[field].includes(value)
+        ? prevState[field].filter((item) => item !== value)
+        : [...prevState[field], value],
     }));
   };
 
@@ -31,10 +35,10 @@ function Form() {
 
   return (
     <>
-      <div className="container">
-        <h2>Plan your Epic Trip</h2>
+      <div className="container-form">
+        <h1>Plan your Epic Trip</h1>
       </div>
-      <div className="container">
+      <div className="container-form">
         <form onSubmit={handleSubmit}>
           <TextField
             label="Where From"
@@ -65,30 +69,160 @@ function Form() {
             margin="normal"
           />
           <Box mt={2}>
-            Food:
-            <Button
-              variant={
-                formData.food.includes("Japanese") ? "contained" : "outlined"
-              }
-              color="primary"
-              onClick={() => toggleFoodSelection("Japanese")}
+            <Typography
+              sx={{ fontFamily: '"Poppins", sans-serif', marginBottom: "5px" }}
             >
-              Japanese
-            </Button>
-            <Button
-              variant={
-                formData.food.includes("Italian") ? "contained" : "outlined"
-              }
-              color="primary"
-              onClick={() => toggleFoodSelection("Italian")}
-            >
-              Italian
-            </Button>
-            {/* ... add more buttons as needed */}
+              Food:
+            </Typography>
+            {[
+              "Japanese",
+              "Chinese",
+              "American",
+              "Italian",
+              "Mediterranean",
+            ].map((food) => (
+              <Button
+                variant={
+                  formData.food.includes(food) ? "contained" : "outlined"
+                }
+                color="primary"
+                onClick={() => toggleSelection("food", food)}
+                key={food}
+                sx={{
+                  borderRadius: "14px",
+                  color: formData.food.includes(food) ? "white" : "#281e41",
+                  backgroundColor: formData.food.includes(food)
+                    ? "#281e41"
+                    : "white",
+                  padding: "10px 20px",
+                  textDecoration: "none",
+                  border: formData.food.includes(food) ? "none" : "solid",
+                  fontSize: "1rem",
+                  fontWeight: "lighter",
+                  fontFamily: '"Poppins", sans-serif',
+                  marginRight: "10px",
+                  "&:last-child": {
+                    marginRight: "0",
+                  },
+                }}
+              >
+                {food}
+              </Button>
+            ))}
           </Box>
           <Box mt={2}>
-            <Button type="submit" variant="contained" color="primary">
-              Submit
+            <Typography
+              sx={{ fontFamily: '"Poppins", sans-serif', marginBottom: "5px" }}
+            >
+              Hobbies:
+            </Typography>
+            {["Sight-seeing", "Hiking", "Reading", "Sport"].map((hobby) => (
+              <Button
+                variant={
+                  formData.hobbies.includes(hobby) ? "contained" : "outlined"
+                }
+                color="primary"
+                onClick={() => toggleSelection("hobbies", hobby)}
+                key={hobby}
+                sx={{
+                  borderRadius: "14px",
+                  color: formData.hobbies.includes(hobby) ? "white" : "#281e41",
+                  backgroundColor: formData.hobbies.includes(hobby)
+                    ? "#281e41"
+                    : "white",
+                  padding: "10px 20px",
+                  textDecoration: "none",
+                  border: formData.hobbies.includes(hobby) ? "none" : "solid",
+                  fontSize: "1rem",
+                  fontWeight: "lighter",
+                  fontFamily: '"Poppins", sans-serif',
+                  marginRight: "10px",
+                  "&:last-child": {
+                    marginRight: "0",
+                  },
+                }}
+              >
+                {hobby}
+              </Button>
+            ))}
+          </Box>
+          <Box mt={2}>
+            <Typography
+              sx={{ fontFamily: '"Poppins", sans-serif', marginBottom: "5px" }}
+            >
+              Places:
+            </Typography>
+            {[
+              "Museums",
+              "Theatre",
+              "Beach",
+              "Hidden Gems",
+              "Must-see Attractions",
+            ].map((place) => (
+              <Button
+                variant={
+                  formData.places.includes(place) ? "contained" : "outlined"
+                }
+                color="primary"
+                onClick={() => toggleSelection("places", place)}
+                key={place}
+                sx={{
+                  borderRadius: "14px",
+                  color: formData.places.includes(place) ? "white" : "#281e41",
+                  backgroundColor: formData.places.includes(place)
+                    ? "#281e41"
+                    : "white",
+                  padding: "10px 20px",
+                  textDecoration: "none",
+                  border: formData.places.includes(place) ? "none" : "solid",
+                  fontSize: "1rem",
+                  fontWeight: "lighter",
+                  fontFamily: '"Poppins", sans-serif',
+                  marginRight: "10px",
+                  "&:last-child": {
+                    marginRight: "0",
+                  },
+                }}
+              >
+                {place}
+              </Button>
+            ))}
+          </Box>
+          <Box mt={2}>
+            <TextField
+              label="What else would you want us to take into consideration? (Optional)"
+              variant="outlined"
+              name="other"
+              value={formData.other}
+              onChange={handleInputChange}
+              fullWidth
+            />
+          </Box>
+          <Box mt={2}>
+            <Button
+              className="btn"
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{
+                borderRadius: "14px",
+                color: "white",
+                backgroundColor: "#281e41",
+                padding: "10px 20px",
+                textDecoration: "none",
+                border: "none",
+                fontSize: "1rem",
+                fontWeight: "lighter",
+                fontFamily: '"Poppins", sans-serif',
+                marginRight: "10px",
+                marginBottom: "30px",
+                "&:last-child": {
+                  marginRight: "0",
+                },
+                marginTop: "20px",
+              }}
+            >
+              Let the magic begin
             </Button>
           </Box>
         </form>
