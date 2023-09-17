16b3cc36d83546311914fd5f747ad18f4086a789
nate <70911747+nathanallen242@users.noreply.github.com>
2023-09-16 21:17:18 -0400

    api

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@mui/material';
 import Rating from '@mui/material/Rating';
@@ -7,10 +7,24 @@ import './map.css';
 const MapComponent = ({ center, zoom, value }) => {
     
     const isMobile = useMediaQuery('(max-width:600px)');
+    const mapRef = useRef(null);
+    const mapsRef = useRef(null);
   
-    const renderMarkers = (map, maps) => {
-      // You can add markers here using the map and maps objects
+    const handleApiLoaded = ({ map, maps }) => {
+        mapRef.current = map;
+        mapsRef.current = maps;
     };
+
+    useEffect(() => {
+      if (mapRef.current && mapsRef.current) {
+          const miamiLatLng = { lat: 25.7617, lng: -80.1918 };  // Miami's coordinates
+          new mapsRef.current.Marker({
+              position: miamiLatLng,
+              map: mapRef.current,
+              title: "Miami!",
+          });
+      }
+  }, [mapRef.current, mapsRef.current]);
   
     return (
       <div className="mapContainer">
@@ -18,7 +32,7 @@ const MapComponent = ({ center, zoom, value }) => {
           bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_API_KEY }}
           defaultCenter={center}
           defaultZoom={zoom}
-          onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+          onGoogleApiLoaded={handleApiLoaded}
           yesIWantToUseGoogleMapApiInternals
         >
           {/* You can add child components here which will act as markers */}
@@ -33,14 +47,14 @@ const MapComponent = ({ center, zoom, value }) => {
         ) : null}
       </div>
     );
-  };
-  
-  MapComponent.defaultProps = {
+};
+
+MapComponent.defaultProps = {
     center: {
-      lat: 25.907,
-      lng: -80.138,
+        lat: 25.907,
+        lng: -80.138,
     },
     zoom: 11,
-  };
-  
-  export default MapComponent;
+};
+
+export default MapComponent;
